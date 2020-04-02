418eb76b38e85d932a32c186715032c08225be80
dlynnea <danimatton@gmail.com>
2020-04-02 17:01:57 -0600

    posts can be liked

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -94,4 +94,49 @@ router.delete('/:id', auth, async (req, res) => {
     }
 });
 
+// like post
+
+router.put('/like/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if(post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+            return res.status(400).json({ msg: 'Cannot like post again' });
+        }
+
+        post.likes.unshift({ user: req.user.id })
+        await post.save();
+
+        res.json(post.likes);
+
+    } catch(err) {
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
+});
+
+// unlike the post
+
+router.put('/unlike/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if(post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
+            return res.status(400).json({ msg: "Post hasn't yet been liked" });
+        }
+
+        const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
+
+        post.likes.splice(removeIndex, 1);
+        
+        await post.save();
+
+        res.json(post.likes);
+
+    } catch(err) {
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
+})
+
 module.exports = router;
