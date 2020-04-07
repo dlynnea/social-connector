3aa9ad0263423947cb83c9565df2149626502d94
dlynnea <danimatton@gmail.com>
2020-04-06 18:31:42 -0600

    can create and delete posts

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,37 +3,38 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Moment from 'react-moment';
+import { addLike, removeLike, deletePost } from '../../actions/post';
 
-const PostItem = ({ auth, post: { _id, text, name, avatar, user, likes, comments, date }}) => {
+const PostItem = ({ addLike, removeLike, deletePost, auth, post: { _id, text, name, avatar, user, likes, comments, date }}) => {
     return (
         <div className="post bg-white p-1 my-1">
             <div>
-                <a href="#">
+                <Link to={`/profile/${user}`}>
                 <img className="round-img" src={avatar} alt="" />
                     <h4>{name}</h4>
-                </a>
+                </Link>
             </div>
             <div>
                 <p className="my-1">
                     {text}
                 </p>
-                <p className="post-date">Posted: <Moment format="DD/MM/YYYY">{date}</Moment></p>
-                <button type="button" className="btn btn-light">
+                <p className="post-date">Posted: <Moment format="MM/DD/YYYY">{date}</Moment></p>
+                <button onClick={() => addLike(_id)} type="button" className="btn btn-light">
                     <i className="fas fa-thumbs-up"></i>
                     <span>{likes.length > 0 && (
                         <span> {likes.length}</span>
                     )}</span>
                 </button>
-                <button type="button" className="btn btn-light">
+                <button onClick={() => removeLike(_id)} type="button" className="btn btn-light">
                     <i className="fas fa-thumbs-down"></i>
                 </button>
-                <Link to={`/post/${_id}`} className="btn btn-primary">
+                <Link to={`/posts/${_id}`} className="btn btn-primary">
                     Discussion {comments.length > 0 && (
                         <span className="comment-count">{comments.length}</span>
                     )}
                 </Link>
                 {!auth.loading && user === auth.user._id && (
-                <button type="button" className="btn btn-danger">
+                <button onClick={(event) => deletePost(_id)} type="button" className="btn btn-danger">
                     <i className="fas fa-times"></i>
                 </button>
                 )
@@ -46,10 +47,13 @@ const PostItem = ({ auth, post: { _id, text, name, avatar, user, likes, comments
 PostItem.propTypes = {
     post: PropTypes.object.isRequired,
     auth: PropTypes.object.isRequired,
+    addLike: PropTypes.func.isRequired,
+    removeLike: PropTypes.func.isRequired,
+    deletePost: PropTypes.func.isRequired,
 }
 
 const mapStateToProps = state => ({
     auth: state.auth
 })
 
-export default connect(mapStateToProps, {})(PostItem);
+export default connect(mapStateToProps, { addLike, removeLike, deletePost })(PostItem);
--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -4,8 +4,9 @@ import Wheel from '../layout/Wheel';
 import { connect } from 'react-redux';
 import { getPosts } from '../../actions/post';
 import PostItem from './PostItem';
+import PostForm from './PostForm';
 
-const Posts = ({ getPosts, post: { post, loading }}) => {
+const Posts = ({ getPosts, post: { posts, loading }}) => {
     useEffect(() => {
         getPosts();
     }, [getPosts]);
@@ -17,9 +18,9 @@ const Posts = ({ getPosts, post: { post, loading }}) => {
             <p className="lead">
                 <i className="fas fa-user"></i> Welcome!
             </p>
-
+            <PostForm />
             <div className="posts">
-                {Posts.map(post => (
+                {posts.map(post => (
                     <PostItem key={post._id} post={post} />
                 ))}
             </div>
