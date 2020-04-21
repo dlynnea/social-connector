bb0cb73ddcfbac90f5f2ad35a1cb2fda73a770f2
dlynnea <danimatton@gmail.com>
2020-04-20 18:34:58 -0600

    cleanup footer

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 const Footer = () => <footer className='footer'>
         <p> dotJoin &copy; {new Date().getFullYear()}</p>
-        {/* <p>[ A JOIN is a means for combining columns (people) from one or more tables (backgrounds) by using values (interests) common to each ]</p> */}
 </footer>
 
 export default Footer;
