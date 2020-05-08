2d28b647fd5a1f90c4e320bf2fbe6c55594acbe0
dlynnea <danimatton@gmail.com>
2020-05-07 18:26:07 -0600

    removing multer library

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -6,37 +6,11 @@ const request = require('request');
 const config = require('config');
 const normalize = require('normalize-url');
 
-const multer = require('multer');
 const mongoose = require('mongoose');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 const Post = require('../../models/Post');
-
-// multer img upload
-const DIR = './public/';
-
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, DIR);
-    },
-    filename: (req, file, cb) => {
-        const fileName = file.originalname.toLowerCase().split(' ').join('-');
-        cb(null, uuidv4() + '-' + fileName)
-    }
-});
-
-var upload = multer({
-    storage: storage,
-    fileFilter: (req, file, cb) => {
-        if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
-            cb(null, true);
-        } else {
-            cb(null, false);
-            return cb(new Error('Only .png, .jpg, and .jpeg format allowed'))
-        }
-    }
-})
 
 // get current user profile
 
